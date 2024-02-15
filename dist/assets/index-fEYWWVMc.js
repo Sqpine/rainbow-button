a839f973f9580cafcb233aef4cfd4428fd3cfdce
Oleksandr Pasikhov <oleksandrpasikhov@gmail.com>
2024-02-15 21:18:17 +0200

    fix: 5

--- dist/assets/index-fEYWWVMc.js
+++ dist/assets/index-fEYWWVMc.js
@@ -1,4 +1,4 @@
-import{n as fe,s as Ke,T as O,t as L,a as g,o as Y,R as Nr,p as H,y as ae}from"./index-CRh9lJSg.js";import{j as Ur}from"./index-CPyfe1NY.js";/**
+import{n as fe,s as Ke,T as O,t as L,a as g,o as Y,R as Nr,p as H,y as ae}from"./index-Doy6HzvD.js";import{j as Ur}from"./index-CylApVLs.js";/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
